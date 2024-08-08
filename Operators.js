f82f1aedc4d655d287d95b69a6fcf0152f6a1d45
Madannagari Raju <madannagariraju@gmail.com>
2024-08-08 08:19:55 +0530

    operator

---
+++ Operators.js
@@ -0,0 +1,51 @@
+let a =4;
+let b=2;
+
+//Arithmetic operator
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log("a" ,a);
+console.log("++a", ++a);
+console.log("a++", a++);
+console.log("--a", --a);
+console.log("a--", a--);
+console.log("a", a);
+console.log("a--", a--);
+
+//Assignment operator
+
+let assign =1;
+assign +=2;
+console.log(assign);
+assign -=2;
+console.log(assign);
+assign *=2
+console.log(assign);
+assign /=2
+console.log(assign);
+assign %=2
+console.log(assign);
+assign **=2
+console.log(assign);
+
+
+//comparsion operator
+
+let c=6;
+let d="6";
+
+console.log("c==d", c==d)
+console.log("c!=d", c!=d)
+console.log("c===d", c===d)  // equal value and type
+console.log("c!==d", c!==d)
+// <, >, <=, >=, ?
+
+
+//logical operator
+
+//&& , ||, !
